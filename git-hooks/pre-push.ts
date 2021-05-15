09994785a5f27950beb5353915ec730d58fc4f0e
ddonovan <info@promatrixinc.com>
2021-05-15 12:10:41 -0400

    ...

--- git-hooks/pre-push.ts
+++ git-hooks/pre-push.ts
@@ -4,6 +4,7 @@ export class PrePush {
 
     constructor() {
       try {
+          
         const noop = new TaskNpmPublish(false, 'ngx-modelling', 'npm', '..\\..\\NgResources\\ngx-modelling', 'library', 'projects\\ng2-express\\dist', '..\\..\\NgResources\\project-control,..\\..\\Angular.Studio.11.00\\studio.app,..\\..\\Angular.Studio.11.00\\studio.test', 'package-ng2-express');
             // we can only see the console.log is the process.exit(1);
             // process.exit(1);
